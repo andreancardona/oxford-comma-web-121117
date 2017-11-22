fca7dfaec930c40ed5a0432fe42b42b01dbe0778
andreancardona@gmail.com <andreancardona@gmail.com>
2017-11-22 19:59:32 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,10 +6,7 @@ def oxford_comma(array)
   if array.length < 2
     array.join
   elsif array.length == 3
-     array = array.join(", ")
-     array.insert(-10,"and ")
-   elsif array.length == 5
-     array = array.join(", ")
-     array.insert(-14, "and ")
+     array.join(", ")
+     array.insert(-1,"and ")
    end
 end
